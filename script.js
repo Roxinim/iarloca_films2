3233c19f9237ffa6f26c58528b6ec4f407833ccd
iarly iandeyare <98906114+Roxinim@users.noreply.github.com>
2022-06-07 18:07:25 -0300

    update

--- script.js
+++ script.js
@@ -260,3 +260,26 @@ function zoom(){
 
     })
 }
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+            display.textContent = minutes + ":" + seconds;
+            if (--timer < 0) {
+                timer = duration;
+            }
+            if (timer == 0){
+                window.location.replace("index.html");
+            }
+        }, 1000);
+    }
+
+    // window.onload = 
+    function timer() {
+        var duration = 60 * 5; // Converter para segundos
+        var display = document.querySelector('#mostrador'); // selecionando o timer
+        startTimer(duration, display); // iniciando o timer
+    };
